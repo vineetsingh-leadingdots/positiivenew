38a4329ada2f33f1691102376c96049fe0376163
NikhilleadingDots <nikhil@leadingdots.com>
2024-03-07 19:09:58 +0530

    NotificationHandler on error

--- src/commonContent.js/commonErrorHandler.js
+++ src/commonContent.js/commonErrorHandler.js
@@ -1,10 +1,17 @@
 
+import { notification } from "antd";
 import { user } from "../redux/slice/authSlice";
-
-
 export const commonErrorHandler = (error, dispatch) => {
     if (error?.status === 401) {
-      console.log("Error: ", error, error?.status);
+      const config=({
+        placement: 'top',
+        duration: 2,
+        closeIcon:true,
+        description: `${error.data.message}`,
+        threshold:2
+      });
+      notification.error(config)
+     
       dispatch(user('Unauthorized - Please login'));
     } else if (error?.error?.status === 404) {
       dispatch(user('Resource not found'));
