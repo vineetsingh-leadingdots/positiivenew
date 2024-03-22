90f25c328f4d741e789861273a4de849c5fe57ed
Pramodkumar932 <pramodkumar.ldots@gmail.com>
2024-03-22 13:07:49 +0530

    new code

--- src/pages/calendar/calendar.js
+++ src/pages/calendar/calendar.js
@@ -32,10 +32,35 @@ const CalendarView = () => {
             <div className="content w-full calendarPage">
                 <div className="calendarPageIn mx-auto  relative ">
                     <CreateNewDropdown />
+                    <div className='timeCard '>
+                        <p>Enter time to meet customer</p>
+                        <div className='timeCardBox'>
+                            <div className='timeItem'>
+                                1
+                            </div>
+                            <div className='timeDots'>
+                                :
+                            </div>
+                            <div className='timeItem'>
+                                00
+                            </div>
+                            <div className='timeItem'>
+                                <div className='timeZoneSwitch'>
+                                    <label><input type='radio' checked name='AM' /><span>AM</span></label>
+                                    <label><input type='radio' name='AM' /><span>PM</span></label>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="timeBoxBtn">
+                            <button className="timeBtn cancel">Cancel</button>
+                            <button className="timeBtn ">Ok</button>
+                        </div>
+                    </div>
+                    <br/>
                     <Popup1 />
                     <Popup2 />
-                    <div className="card pt-8">                      
-                    <CustomCalendar/>                  
+                    <div className="card pt-8">
+                        <CustomCalendar />
                     </div>
                 </div>
             </div>
