d064267a3cd8bb87ff9e1aa2758506a053c00d34
sankitdagarldots29 <162310379+sankitdagarldots29@users.noreply.github.com>
2024-03-22 16:31:02 +0530

    error fix es-lint

--- src/pages/supplier/supplier.js
+++ src/pages/supplier/supplier.js
@@ -1,68 +1,65 @@
-import React from, { useEffect } "react";
+import React from "react";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import SearchField from "../../components/searchFIeld";
 import AddButton from "../../components/addButton";
 import TableList from "../../commonComponents/tableList";
-import {
-  SupplierAdminColumns,
-  SupplierAdminData,
-} from "../../commonComponents/tableData";
-
+import { SupplierAdminColumns } from "../../commonComponents/tableData";
+import { useEffect } from "react";
+import { useListsupplierQuery } from "../../services/supplierApi";
+import { Link } from "react-router-dom";
+import SupplierDelete from "../../components/supplierDelete";
 const Supplier = () => {
+  const { data: supplierListData, refetch } = useListsupplierQuery();
 
-    const { data: supplierListData, refetch } = useListsupplierQuery();
+  useEffect(() => {
+    refetch();
+  }, [supplierListData]);
 
+  const tableData = supplierListData?.data.map((supplier) => ({
+    key: supplier?.suppliers ? supplier?.id?.toString() : "",
+    SupplierName: supplier?.suppliers
+      ? supplier?.suppliers?.User?.fullName
+      : "",
+    Email: supplier?.suppliers ? supplier?.suppliers?.User?.email : "",
+    Actions: (
+      <div className="flex gap-1">
+        <Link to="/supplier/add" className="actionButton">
+          <i className="fa fa-pencil" />
+        </Link>
+        <Link to="/supplier/view" className="actionButton">
+          <i className="fa fa-eye" />
+        </Link>
+        {/* <DeletePopup/> */}
+        <SupplierDelete deleteId={supplier?.id} refetch={refetch} />
+      </div>
+    ),
+  }));
 
-     useEffect(() => {
-        refetch();
-    }, [ supplierListData ]);
-
-    console.log(supplierListData);
-    const tableData = supplierListData?.data.map((supplier) => ({
-        key: supplier?.suppliers ? supplier?.id?.toString() : '',
-        SupplierName: supplier?.suppliers ? supplier?.suppliers?.User?.fullName : '',
-        Email: supplier?.suppliers ? supplier?.suppliers?.User?.email : '',
-        Actions:  (
-            <div className="flex gap-1">
-                <Link to="/supplier/add" className="actionButton"><i className="fa fa-pencil"/></Link>
-                <Link to="/supplier/view" className="actionButton"><i className="fa fa-eye"/></Link>
-                {/* <DeletePopup/> */}
-                <SupplierDelete 
-                    deleteId={supplier?.id}
-                    refetch={refetch}
-                />
+  return (
+    <>
+      <HelmetProvider>
+        <Helmet>
+          <title>Positiive | Suppliers </title>
+        </Helmet>
+      </HelmetProvider>
+      <div className="content">
+        <div className="w-full mb-4">
+          <h2 className="mb-4"> Supplier</h2>
+          <div className="flex justify-between searchWithButton">
+            <SearchField placeholder="Search by name" />
+            <div className="btnRight">
+              <AddButton linkHref="/supplier/add" btnText="Add Supplier" />
             </div>
-        ),
-    }));
-
-    return (
-        <>
-            <HelmetProvider>
-                <Helmet>
-                    <title>Positiive | Suppliers </title>
-                </Helmet>
-            </HelmetProvider>
-            <div className='content'>
-                <div className='w-full mb-4'>
-                    <h2 className='mb-4'> Supplier</h2>
-                    <div className='flex justify-between searchWithButton'>
-                        <SearchField placeholder="Search by name" />
-                        <div className='btnRight'>
-                            <AddButton
-                                linkHref="/supplier/add"
-                                btnText="Add Supplier"
-                            />
-                        </div>
-                    </div>
-                </div>
-                <div className='card '>
-                    <div className='tableAreaMaster '>
-                        <TableList data={tableData} columns={SupplierAdminColumns} />
-                    </div>
-                </div>
-            </div>
-        </>
-    )
-}
+          </div>
+        </div>
+        <div className="card ">
+          <div className="tableAreaMaster ">
+            <TableList data={tableData} columns={SupplierAdminColumns} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Supplier;
