d063c0cee26b8ee29697e299f35dddc55092321f
Yash <yashvarshney2001@gmail.com>
2024-03-26 16:16:42 +0530

    removed_eslint_err

--- src/pages/role/roleAdd.js
+++ src/pages/role/roleAdd.js
@@ -1,75 +1,77 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 // import {useNavigate} from "react-router-dom";
-// import { Helmet, HelmetProvider } from "react-helmet-async";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 // import { useCreateroleMutation } from '../../services/roleApi';
 // import {  useLocation, useNavigate  } from 'react-router-dom';
 
 const RoleAdd = () => {
   const navigate = useNavigate();
 
-  return (
-    <>
-      <HelmetProvider>
-        <Helmet>
-          <title>Positiive | Role Add </title>
-        </Helmet>
-      </HelmetProvider>
-      <div className="content masterPages">
-        <div className="w-full mb-4">
-          <h2 className="mb-4"> Role Add</h2>
-        </div>
-        <div className="card p-8 ">
-          <div className="formArea mt-5">
-            <form id="brandAdd">
-              <div className=" w-full ">
-                <div className="form-group mb-6 ">
-                  <label className="formBlock mb-2 py-2">Name *</label>
-                  <input
-                    id="name"
-                    name="name"
-                    type="text"
-                    placeholder="Enter brand name"
-                    className="formControl"
-                  />
-                </div>
-              </div>
-              <div className=" w-full ">
-                <div className="form-group mb-6 ">
-                  <label className="formBlock mb-2 py-2">Description</label>
-                  <textarea
-                    id="name"
-                    name="name"
-                    type="text"
-                    placeholder="Enter role description"
-                    className="formControl"
-                  />
-                </div>
-              </div>
-              <div className="w-full mb-6">
-                <h2>Permission *</h2>
-              </div>
-              <div className="w-full formFooter">
-                <div className=" form-group pt-8 pb-3 flex  gap-3 ">
-                  <button type="submit" className=" btn-save btnSm ">
-                    Submit
-                  </button>
-                  <button
-                    type="button"
-                    onClick={() => navigate("/role/list")}
-                    className="btn-cancel btnSm "
-                  >
-                    Cancel
-                  </button>
-                </div>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-};
+    
+//   return (
+//     <>
+//       <HelmetProvider>
+//         <Helmet>
+//           <title>Positiive | Role Add </title>
+//         </Helmet>
+//       </HelmetProvider>
+//       <div className="content masterPages">
+//         <div className="w-full mb-4">
+//           <h2 className="mb-4"> Role Add</h2>
+//         </div>
+//         <div className="card p-8 ">
+//           <div className="formArea mt-5">
+//             <form id="brandAdd">
+//               <div className=" w-full ">
+//                 <div className="form-group mb-6 ">
+//                   <label className="formBlock mb-2 py-2">Name *</label>
+//                   <input
+//                     id="name"
+//                     name="name"
+//                     type="text"
+//                     placeholder="Enter brand name"
+//                     className="formControl"
+//                   />
+//                 </div>
+//               </div>
+//               <div className=" w-full ">
+//                 <div className="form-group mb-6 ">
+//                   <label className="formBlock mb-2 py-2">Description</label>
+//                   <textarea
+//                     id="name"
+//                     name="name"
+//                     type="text"
+//                     placeholder="Enter role description"
+//                     className="formControl"
+//                   />
+//                 </div>
+//               </div>
+//               <div className="w-full mb-6">
+//                 <h2>Permission *</h2>
+//               </div>
+//               <div className="w-full formFooter">
+//                 <div className=" form-group pt-8 pb-3 flex  gap-3 ">
+//                   <button type="submit" className=" btn-save btnSm ">
+//                     Submit
+//                   </button>
+//                   <button
+//                     type="button"
+//                     onClick={() => navigate("/role/list")}
+//                     className="btn-cancel btnSm "
+//                   >
+//                     Cancel
+//                   </button>
+//                 </div>
+//               </div>
+//             </form>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
   
     // const [ createrole, { isSuccess:createroleSuccess } ] = useCreateroleMutation();
     // const navigate = useNavigate();
@@ -166,7 +168,7 @@ const RoleAdd = () => {
                                     <button
                                         type="submit"
                                         className=" btn-save btnSm "
-                                        onClick={handelSubmit}
+                                        // onClick={handelSubmit}
                                     >
                                         Submit
                                     </button>
@@ -184,7 +186,7 @@ const RoleAdd = () => {
                 </div>
             </div>
         </>
-    )
-}
+    );
+};
 
 export default RoleAdd;
